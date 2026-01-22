a5e650de205e470023ab3e9934a81458cbf68faf
m1v8a <245783116+m1v8a@users.noreply.github.com>
2026-01-22 13:02:01 +0800

    feat(rework): Add ugly UI
    The UI is working properly but it's ugly.
    It changes screens from "start screen" > "game screen" > "game over
    screen" on certain conditions

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 
 (function main() {
-	const players = { playerOne: null, playerTwo: null };
 	const TICTACTOE = createTicTacToe();
-	const UI = createUI();
+	const UI_CONTROLLER = createUIController();
 
+	UI_CONTROLLER.setControls();
+	UI_CONTROLLER.setStartButtonHandler((inputValues) => {
+		const playerOne = TICTACTOE.createPlayer(inputValues.name, "x");
+		const playerTwo = TICTACTOE.createPlayer("Computer", "o");
+		TICTACTOE.setPlayers(playerOne, playerTwo);
+		UI_CONTROLLER.switchScreen("game");
+	});
+
+	UI_CONTROLLER.setGameBoardHandler((y, x) => {
+		const currentTurn = TICTACTOE.getState().currentTurn;
+		TICTACTOE.play(currentTurn.getMarker(), y, x);
+		
+		if(TICTACTOE.getState().isGameOver) {
+			UI_CONTROLLER.switchScreen("over");
+		}
+
+	});
+
+	UI_CONTROLLER.switchScreen("start");
+	UI_CONTROLLER.updateBoard(TICTACTOE.getBoard());
 })();
 
-function createPlayer(name, marker) {
-	const getMarker = () => marker;
-	const getName = () => name;
-	return { getName, getMarker }
-}
 
 
-function logBoard(board, result) {
+function logBoard(board, state) {
 	console.clear();
 	for (let i = 0; i < board.length; i++) {
 		console.log(board[i]);
 	}
-	console.log(result)
+	console.log(state);
 }
 
 
@@ -26,9 +40,22 @@ function createTicTacToe() {
 	const BOARD_SIZE = 3;
 	const X = "x";
 	const O = "o";
-	let result = null;
+	const players = {playerOne: null, playerTwo: null};
+	let state = { isGameOver: false, winner: null, currentTurn: players.playerOne };
 	let board = createBoard();
 
+	function createPlayer(name, marker) {
+		const getMarker = () => marker;
+		const getName = () => name;
+		return { getName, getMarker }
+	}
+
+	function setPlayers(playerOne, playerTwo) {
+		players.playerOne = playerOne;
+		players.playerTwo = playerTwo;
+		state.currentTurn = players.playerOne;
+	}
+	
 	function createBoard() {
 		const b = [];
 		for (let y = 0; y < BOARD_SIZE; y++) {
@@ -41,10 +68,19 @@ function createTicTacToe() {
 		return b;
 	}
 
+	function getBoard() {
+		return board;
+	}
+
+	function getState() {
+		return state;
+	}
+
 	function play(marker, y, x) {
 		board[y][x] = marker;
 		check();
-		logBoard(board, result);
+		switchTurn();
+		logBoard(board, state);
 	}
 
 	function check() {
@@ -67,11 +103,14 @@ function createTicTacToe() {
 			const b2 = board[+c2[0]][+c2[1]];
 			const b3 = board[+c3[0]][+c3[1]];	
 			if (b1 === O && b2 === O && b3 === O) {
-				result = O;	
+				state.isGameOver = true;	
+				state.winner = O;	
 			} else if (b1 === X && b2 === X && b3 === X) {
-				result = X;	
+				state.isGameOver = true;	
+				state.winner = X;	
 			} else if(isDraw()) {
-				result = "draw";
+				state.isGameOver = true;
+				state.winner = null;
 			}
 		}
 	}
@@ -89,11 +128,93 @@ function createTicTacToe() {
 		return false;
 	}
 
-	return { play }
+
+	function switchTurn() {
+		const currentTurn = state.currentTurn;
+		const playerOne = players.playerOne;
+		const playerTwo = players.playerTwo;
+		if(playerOne.getMarker() === currentTurn.getMarker()) {
+			state.currentTurn = playerTwo;
+		} else {
+			state.currentTurn = playerOne;
+		}
+	}
+
+
+	return { play, getBoard, getState, createPlayer, setPlayers };
 }
 
 
-function createUI() {
+function createUIController() {
+	const boardEl = document.querySelector("#game-board");
+	const inputValues = { name: "", };
+	let gameBoardHandler = null;
+	let startButtonHandler = null;
+
+	const screens = {
+		start: document.querySelector("#start-screen"),
+		game: document.querySelector("#game-screen"),
+		over: document.querySelector("#game-over-screen"),
+	}
+	
+	function setControls() {
+		const startButton = document.querySelector("#start-button");
+		const nameInputEl = document.querySelector("#name");
+
+		startButton.addEventListener("click", () => {
+			startButtonHandler(inputValues);
+		});
+
+		nameInputEl.addEventListener("input", () => {
+			inputValues.name = nameInputEl.value
+		})
+
+		boardEl.addEventListener("click", (e) => {
+			const t = e.target;
+			if(t.classList.contains("cell")) {
+				const y = t.dataset.y;
+				const x = t.dataset.x;
+				gameBoardHandler(y, x);
+			}
+		});
+	}
+
+	function setGameBoardHandler(fn) {
+		gameBoardHandler = fn;
+	}
+
+	function setStartButtonHandler(fn) {
+		startButtonHandler = fn;
+	}
+
+	function updateBoard(board) {
+		boardEl.innerHTML = "";
+		board.forEach((y, yi) => {
+			y.forEach((x, xi) => {
+				const cell = document.createElement("div");
+				cell.className = "cell";
+				cell.dataset.y = yi;
+				cell.dataset.x = xi;
+				boardEl.appendChild(cell);
+			});
+		});
+	}
+
+	function switchScreen(key) {
+		for(let k in screens) {
+			screens[k].classList.add("hidden");
+		}
+		screens[key].classList.remove("hidden");
+	}
+
+
+	return { 
+		setControls, 
+		switchScreen, 
+		updateBoard, 
+		setGameBoardHandler,
+		setStartButtonHandler,
+	};
 }
 
 
